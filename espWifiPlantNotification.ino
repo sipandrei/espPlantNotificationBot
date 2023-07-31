a1533963ae5682b9600e6f740ca00bced7321a34
sipandrei <pontos.serban@gmail.com>
2023-07-31 17:31:34 +0300

    telegram API integration

--- espWifiPlantNotification.ino
+++ espWifiPlantNotification.ino
@@ -1,8 +1,16 @@
+#include <UniversalTelegramBot.h>
+#include <WiFiClientSecure.h>
 #include <WiFiManager.h>
 #include <ESP8266WebServer.h>
 #include <credentials.h>
-#include <Pinger.h>
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(PLANT_NOTE_BOT, secured_client);
+const unsigned long SCAN_DELAY = 1000;
+unsigned long lastScan;
 
 extern "C"
 {
@@ -12,10 +20,8 @@ int soilSensorInput, soilSensorPercentage;
 int baselineAir = 680;
 int baselineWater = 280;
 int minimumPercentage = 35;
-
-int webNow;
-
-Pinger pinger;
+bool watered = true;
+bool waitMin = false;
 
 IPAddress local_IP(192, 168, 100, 36);
 IPAddress gateway(192,168,100,1);
@@ -37,14 +43,28 @@ void configModeCallback(WiFiManager *wifiManager){
   Serial.println(WiFi.softAPIP());
 }
 
-/*void setMinimum(Control* sender, int type)
-{
-    minimumPercentage = sender->value.toInt();
-}*/
-
-
-void handleClient() {
-  server.send(200, "text/html", "<h1>Test</h1>"); 
+void messagesHandler(int numNewMessages){
+ Serial.printf("\n Received %d new messages \n", numNewMessages);
+  String response;
+  for(int i = 0; i < numNewMessages; i++){
+    telegramMessage &msg = bot.messages[i];
+    Serial.print("Message " + msg.text);
+    Serial.print(numNewMessages);
+    Serial.print(i);
+    if(msg.text == "/setmin" || msg.text == "/setmin@plantnote_bot"){
+        waitMin = true;
+        response = "Set the minimum humidity  by replying with a number between 5 and 90";
+    } else if(msg.text.toInt() >= 5 && msg.text.toInt() <= 90 && waitMin == true){
+        waitMin = false;
+        minimumPercentage = msg.text.toInt();
+        response = "Minimum percentage set to " + msg.text;
+      } else if(msg.text == "/humidity" || msg.text == "/humidity@plantnote_bot"){
+          response = "Current humidity is " + String(soilSensorPercentage) + "%"; 
+        } else {
+        response = "That was not a valid input";
+      }
+    bot.sendMessage(msg.chat_id, response, "Markdown");
+  }
 }
 
 void setup() {
@@ -58,127 +78,59 @@ void setup() {
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
   res = wifiManager.autoConnect("RobotNotificarePlante", "parolabuna");
-//  wifiManager.setSTAStaticIPConfig(local_IP, gateway, subnet); // optional DNS 4th argument
   Serial.println(WiFi.localIP());
-
-  /*if(res){
-    //dnsServer.start(DNS_PORT,"*", local_IP);
-    webNow = ESPUI.label("Umiditate Sol acum:", ControlColor::Emerald, "");
-        ESPUI.number("Umiditate Minima Dorita", &setMinimum, ControlColor::Alizarin, minimumPercentage, 0, 10);
-
-  }*/
 
   if(!res){
     Serial.print("DID NOT CONNECT");
     ESP.restart();}
-//   else {
-//    Serial.print("Ss");
-//    server.on("/", handleClient);
-//    server.begin();
-//  }
-  //ESPUI.begin("UmiSol");
-    
-  pinger.OnReceive([](const PingerResponse& response)
-  {
-    if (response.ReceivedResponse)
+   else {
+    secured_client.setTrustAnchors(&cert); 
+    Serial.print("Retrieving time: ");
+    configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+    time_t now = time(nullptr);
+    while (now < 24 * 3600)
     {
-      Serial.printf(
-        "Reply from %s: bytes=%d time=%lums TTL=%d\n",
-        response.DestIPAddress.toString().c_str(),
-        response.EchoMessageSize - sizeof(struct icmp_echo_hdr),
-        response.ResponseTime,
-        response.TimeToLive);
+      Serial.print(".");
+      delay(100);
+      now = time(nullptr);
     }
-    else
-    {
-      Serial.printf("Request timed out.\n");
-    }
-
-    // Return true to continue the ping sequence.
-    // If current event returns false, the ping sequence is interrupted.
-    return true;
-  });
-  
-  pinger.OnEnd([](const PingerResponse& response)
-  {
-    // Evaluate lost packet percentage
-    float loss = 100;
-    if(response.TotalReceivedResponses > 0)
-    {
-      loss = (response.TotalSentRequests - response.TotalReceivedResponses) * 100 / response.TotalSentRequests;
-    }
-    
-    // Print packet trip data
-    Serial.printf(
-      "Ping statistics for %s:\n",
-      response.DestIPAddress.toString().c_str());
-    Serial.printf(
-      "    Packets: Sent = %lu, Received = %lu, Lost = %lu (%.2f%% loss),\n",
-      response.TotalSentRequests,
-      response.TotalReceivedResponses,
-      response.TotalSentRequests - response.TotalReceivedResponses,
-      loss);
-
-    // Print time information
-    if(response.TotalReceivedResponses > 0)
-    {
-      Serial.printf("Approximate round trip times in milli-seconds:\n");
-      Serial.printf(
-        "    Minimum = %lums, Maximum = %lums, Average = %.2fms\n",
-        response.MinResponseTime,
-        response.MaxResponseTime,
-        response.AvgResponseTime);
-    }
-    
-    // Print host data
-    Serial.printf("Destination host data:\n");
-    Serial.printf(
-      "    IP address: %s\n",
-      response.DestIPAddress.toString().c_str());
-    if(response.DestMacAddress != nullptr)
-    {
-      Serial.printf(
-        "    MAC address: " MACSTR "\n",
-        MAC2STR(response.DestMacAddress->addr));
-    }
-    if(response.DestHostname != "")
-    {
-      Serial.printf(
-        "    DNS name: %s\n",
-        response.DestHostname.c_str());
-    }
-
-    return true;
-  });
-  
-  // Ping default gateway
-  Serial.printf(
-    "\n\nPinging default gateway with IP %s\n",
-    WiFi.gatewayIP().toString().c_str());
-  if(pinger.Ping(WiFi.gatewayIP()) == false)
-  {
-    Serial.println("Error during last ping command.");
+    Serial.println(now);
+    Serial.print("Sending message to ");
+    Serial.println(CHAT_ID);
+    bot.sendMessage(CHAT_ID, "Plant bot started, minimum moisture - 35%", "");
   }
-  
-  delay(10000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   soilSensorInput = analogRead(A0);
   soilSensorPercentage = map(soilSensorInput,baselineAir,baselineWater, 0, 100);
   percentageLimiter(soilSensorPercentage);
-  // Serial.println(soilSensorInput);
-  // Serial.print(soilSensorPercentage);
-    Serial.printf("\n\nPinging google.com\n");
-  if(pinger.Ping("google.com") == false)
-  {
-    Serial.println("Error during ping command.");
+    
+  if(millis() - lastScan > SCAN_DELAY){
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    Serial.print(numNewMessages);
+    while (numNewMessages)
+    {
+      Serial.println("got response");
+      messagesHandler(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    Serial.print(numNewMessages);
+    lastScan = millis();
   }
-
-    delay(3000);
-
-  //ESPUI.print(webNow, String(soilSensorPercentage)+"%");
-
- 
+  
+  if(waitMin == false){
+    if(soilSensorPercentage < minimumPercentage && watered == true){
+     watered = false;
+      Serial.print("Plant needs water!! Current humidity: ");
+      Serial.println(soilSensorPercentage);
+      bot.sendMessage(CHAT_ID, "*ALERT!! PLANT NEEDS WATER!!* Current percentage: ", "Markdown");
+      bot.sendMessage(CHAT_ID, String(soilSensorPercentage),"");
+   } else if(soilSensorPercentage >= minimumPercentage && watered == false){
+      watered = true;
+      Serial.print("Plant plant *watered*! Current humidity: ");
+      Serial.println(soilSensorPercentage);
+      bot.sendMessage(CHAT_ID, "Plant *watered*!", "");
+    }
+  } 
 }
